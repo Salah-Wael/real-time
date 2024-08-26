438a5f8b81639b4df06f89e81c89d9a9518f705d
Salah <sasa.wael2016@gmail.com>
2024-08-26 23:23:25 +0300

    initial commit

---
+++ .history/app/Http/Controllers/Auth/RegisteredUserController_20240826003421.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Models\Admin;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Auth\Events\Registered;
+use App\Providers\RouteServiceProvider;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): View
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+        $admin = Admin::find(1);
+        
+
+        //send notification to admin
+
+        Auth::login($user);
+
+        return redirect(RouteServiceProvider::HOME);
+    }
+}
---
+++ .history/resources/views/dashboard/partials/scripts.blade_20240826171936.php
@@ -0,0 +1,48 @@
+@stack('scripts')
+
+<script src="{{ asset('assets') }}/jquery-3.6.0.min.js"></script>
+<script src="{{ asset('assets') }}/js/jquery.min.js"></script>
+<script src="{{ asset('assets') }}/js/popper.min.js"></script>
+<script src="{{ asset('assets') }}/js/moment.min.js"></script>
+<script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets') }}/js/simplebar.min.js"></script>
+<script src='{{ asset('assets') }}/js/daterangepicker.js'></script>
+<script src='{{ asset('assets') }}/js/jquery.stickOnScroll.js'></script>
+<script src="{{ asset('assets') }}/js/tinycolor-min.js"></script>
+<script src="{{ asset('assets') }}/js/config.js"></script>
+<script src="{{ asset('assets') }}/js/d3.min.js"></script>
+<script src="{{ asset('assets') }}/js/topojson.min.js"></script>
+<script src="{{ asset('assets') }}/js/datamaps.all.min.js"></script>
+<script src="{{ asset('assets') }}/js/datamaps-zoomto.js"></script>
+<script src="{{ asset('assets') }}/js/datamaps.custom.js"></script>
+<script src="{{ asset('assets') }}/js/Chart.min.js"></script>
+<script src="{{ asset('assets') }}/js/gauge.min.js"></script>
+<script src="{{ asset('assets') }}/js/jquery.sparkline.min.js"></script>
+<script src="{{ asset('assets') }}/js/apexcharts.min.js"></script>
+<script src="{{ asset('assets') }}/js/apexcharts.custom.js"></script>
+<script src='{{ asset('assets') }}/js/jquery.mask.min.js'></script>
+<script src='{{ asset('assets') }}/js/select2.min.js'></script>
+<script src='{{ asset('assets') }}/js/jquery.steps.min.js'></script>
+<script src='{{ asset('assets') }}/js/jquery.validate.min.js'></script>
+<script src='{{ asset('assets') }}/js/jquery.timepicker.js'></script>
+<script src='{{ asset('assets') }}/js/dropzone.min.js'></script>
+<script src='{{ asset('assets') }}/js/uppy.min.js'></script>
+<script src='{{ asset('assets') }}/js/quill.min.js'></script>
+<script>
+    $(document).ready(function () {
+        //监听注册兼容函数
+        //element为监听对象
+        //eventName监听事件,要加引号(不加on)
+        //fn为回调函数函数
+        function compatibleAddEventListener(element, eventName, fn) {
+            //判断当前浏览器是否支持addEventListener方法
+            if (element.addEventListener) {
+                element.addEventListener(eventName, fn) //第三个参数默认false
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + eventName, fn)
+            } else {
+                element['on' + eventName] = fn
+            }
+        }
+    });
+</script>
